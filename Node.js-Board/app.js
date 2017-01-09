2269c105b77bc2ddbfc7054e3f9e3c9b61784170
DESKTOP-O3HOAJ8\jbin <kjb7907@naver.com>
2017-01-10 02:17:28 +0900

    list 페이징 작업중

--- Node.js-Board/app.js
+++ Node.js-Board/app.js
@@ -20,7 +20,6 @@ app.set('view engine', 'jade'); //템플릿엔진 세팅 express 연결
 
 //글목록
 app.get('/board/list', function(req, res){
-  console.log("list");
   var currentPage = 1; //현재페이지 할당
   var pagePerRow = 10; //가져올 게시물 수 할당
   var totalRowCount = 0; //전체게시물 변수 할당
@@ -32,13 +31,18 @@ app.get('/board/list', function(req, res){
   //전체 게시물 수 가져오기
   var countSql = "SELECT COUNT(*) FROM board";
   conn.query(countSql,function(err,count,fields){
-    totalRowCount = count[0].count;
-    console.log(totalRowCount+" 전체게시글 수");
+    console.log(count); //테스트1
+    console.log(count[0]); //테스트 2
+  //  console.log(count[0].count(*)); //테스트3
+    totalRowCount = count[0];
+    console.log(totalRowCount+" : 전체게시글 수");
   });
 
   //글번호 & 글제목 가져오기
   var sql = 'SELECT board_no,board_title,board_user,board_date FROM board';
   conn.query(sql, function(err, boards, fields){ //쿼리문실행&쿼리문실행후 실행되는 콜백함수
+      console.log(boards[0].board_date); //테스트 1
+      console.log(boards[0].board_date+" : boards[0].board_date"); //테스트 2
       res.render('list', {boards:boards}); //전체글만 전달하며 view페이지 렌더링
   });
 });
